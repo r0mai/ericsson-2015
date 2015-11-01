41012468700dbea2a2c9cc6df1dbd59c9202d407
Andras Kucsma <andras.kucsma@prezi.com>
2015-11-01 16:34:06 +0100

    Add 2.cpp

---
+++ quiz/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    unsigned int b = -10;
+    if (b > a) {
+        std::cout << "a = " << a << std::endl;
+        std::cout << "b = " << b << std::endl;
+        std::cout << "int(b) = " << int(b) << std::endl;
+        std::cout << "universe broken!" << std::endl;
+    }
+}
