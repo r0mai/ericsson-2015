ffc03bf07c1b76d1ff27ee6219265dfc6480d85a
Andras Kucsma <andras.kucsma@prezi.com>
2015-11-07 16:50:03 +0100

    Expand 2.cpp a bit

--- quiz/2.cpp
+++ quiz/2.cpp
@@ -6,6 +6,7 @@ int main() {
     if (b > a) {
         std::cout << "a = " << a << std::endl;
         std::cout << "b = " << b << std::endl;
+        std::cout << "unsigned(a) = " << unsigned(a) << std::endl;
         std::cout << "int(b) = " << int(b) << std::endl;
         std::cout << "universe broken!" << std::endl;
     }
